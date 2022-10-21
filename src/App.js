aeed9bd5d676b3f32376866967f75d5ee392c220
Ana_Rodrigues <ana.pereira@thoughtworks.com>
2022-10-21 00:14:23 -0300

    feat: organizacao do formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Banner from "./Components/Banner/Banner";
+import CampoTexto from "./Components/CampoTexto";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto />
     </div>
   );
 }
